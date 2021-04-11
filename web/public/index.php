63e7a3130d63aa9902aca9d4d8f80301294d2279
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-11 16:33:33 +0500

    week1|add index page

---
+++ web/public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Результаты задний</title>
+</head>
+<body>
+    <h1>Результаты заданий</h1>
+    <ul>
+        <li><a href="task1/result.php">Задание 1</a></li>
+        <li><a href="task2/result.php">Задание 2</a></li>
+        <li><a href="task3/result.php">Задание 3</a></li>
+    </ul>
+</body>
+</html>
