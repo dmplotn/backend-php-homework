0cd896f6a9a36e10308983a7b36a5793ffa2eb55
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-20 22:41:15 +0500

    week2|(task2) add auth manager

---
+++ web/app/task2/AuthManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Task2\AuthManager;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../app/task2/Models/User.php';
+
+use function Task2\Models\User\{getLogin, getPassword};
+
+function isUserSignedIn(): bool
+{
+    return array_key_exists('login', $_SESSION);
+}
+
+function getSignedInUserLogin(): string
+{
+    if (!isUserSignedIn()) {
+        throw new \RuntimeException("User is not signed in");
+    }
+
+    return $_SESSION['login'];
+}
+
+function signIn(array $user): void
+{
+    if (isUserSignedIn()) {
+        throw new \RuntimeException("User is already signed in");
+    }
+    $_SESSION['login'] = getLogin($user);
+}
+
+function signOut()
+{
+    unset($_SESSION['login']);
+    session_destroy();
+}
+
+function canUserSignIn($user, $password)
+{
+    if ($user === null) {
+        return false;
+    }
+
+    return password_verify($password, getPassword($user));
+}
