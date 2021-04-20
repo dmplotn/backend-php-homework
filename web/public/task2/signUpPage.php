06cb125b0590d1537ac3b4015feaeffe9d729594
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-20 22:27:13 +0500

    week2|(task2) add sign up page

---
+++ web/public/task2/signUpPage.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start();
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../app/task2/AuthManager.php';
+
+use function Task2\AuthManager\{isUserSignedIn};
+
+if (isUserSignedIn()) {
+    header('Location: /task2/index.php');
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация пользователя</title>
+    <script src="scripts/errorMessages.js"></script>
+    <script src="scripts/signUp.js"></script>
+</head>
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/task2/inc/header.php' ?>
+    <main>
+        <h1>Регистрация пользователя</h1>
+        <form onsubmit="signUp(); return false">
+            <div>
+                <label for="login">Логин:</label>
+                <input type="text" id="login" name="login">
+                <ul class="errorContainer"></ul>
+            </div>
+            <div>
+                <label for="password">Пароль:</label>
+                <input type="password" id="password" name="password">
+                <ul class="errorContainer"></ul>
+            </div>
+            <div>
+                <label for="passwordConfirmation">Подтверждение пароля:</label>
+                <input type="password" id="passwordConfirmation" name="passwordConfirmation">
+                <ul class="errorContainer"></ul>            
+            </div>
+            <ul class="errorContainer" id="signUp"></ul>
+            <p>
+                <button type="submit">Зарегистрироваться</button>
+            </p>
+        </form>
+    </main>
+</body>
+</html>
