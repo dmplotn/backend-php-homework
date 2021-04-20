24f6d61ca6e504c47deef92243a4fad1adff32ca
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-20 22:57:11 +0500

    week2|(task2) add auth validator

---
+++ web/app/task2/Validators/AuthValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Task2\Validators\AuthValidator;
+
+function validate(string $login, string $password): array
+{
+    $errors = [];
+
+    if (empty($login)) {
+        $errors['login'][] = "Поле 'логин' не должно быть пустым";
+    }
+
+    if (empty($password)) {
+        $errors['password'][] = "Поле 'пароль' не должно быть пустым";
+    }
+
+    return $errors;
+}
