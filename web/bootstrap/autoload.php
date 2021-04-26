250e79463c2bb320268886be5584f8ad14cddb93
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-26 14:39:17 +0500

    week3|update autoloading

--- web/bootstrap/autoload.php
+++ web/bootstrap/autoload.php
@@ -1,6 +1,9 @@
 <?php
 
-set_include_path($_SERVER['DOCUMENT_ROOT'] . '/../app/task1');
+set_include_path(
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task1'
+    . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_1'
+);
 spl_autoload_register(function ($class) {
     include $class . '.php';
 });
