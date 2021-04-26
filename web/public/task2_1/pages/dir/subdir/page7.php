4def6e87a7973dba56ff6954f81df3746f6def66
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-26 21:06:32 +0500

    week3|(task2_1) add page files

---
+++ web/public/task2_1/pages/dir/subdir/page7.php
@@ -0,0 +1,12 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/task2_1/inc/setup.php' ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title><?= $pageName ?></title>
+</head>
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/task2_1/inc/content.php' ?>
+</body>
+</html>
