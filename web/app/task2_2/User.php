e385a00b191511f1ef370e7311398346b01eabda
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-27 13:09:19 +0500

    week3|(task2_2) add user model

---
+++ web/app/task2_2/User.php
@@ -0,0 +1,23 @@
+<?php
+
+class User
+{
+    private $login;
+    private $password;
+
+    public function __construct(string $login, string $password)
+    {
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
