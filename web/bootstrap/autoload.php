6206afbfb66616204de8b03662d59b411843a727
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-28 17:14:57 +0500

    week3|update autoloading

--- web/bootstrap/autoload.php
+++ web/bootstrap/autoload.php
@@ -1,15 +1,20 @@
 <?php
 
 set_include_path(
-    $_SERVER['DOCUMENT_ROOT'] . '/../app/task1'
-    . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_1'
-    . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2'
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task1' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task1/Exceptions' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task1/Validators' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_1' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2/Exceptions' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2/Models' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2/Repositories' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2/Validators' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task3' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task3/Workers' . PATH_SEPARATOR .
+    $_SERVER['DOCUMENT_ROOT'] . '/../app/task3/Validators'
 );
-// set_include_path(
-//     $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2'
-    // . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_1'
-    // . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../app/task2_2'
-// );
+
 spl_autoload_register(function ($class) {
     include $class . '.php';
 });
