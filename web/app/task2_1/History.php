8854ea25073811e8a8530aacbf12b6f7c73e8639
Dmitry Plotnikov <dmplotn@gmail.com>
2021-04-30 00:00:05 +0500

    week3|(task2_1) fix history

--- web/app/task2_1/History.php
+++ web/app/task2_1/History.php
@@ -2,7 +2,7 @@
 
 class History
 {
-    private const HISTORY_SIZE = 3;
+    private const HISTORY_MAX_SIZE = 3;
 
     public static function getHistory(): array
     {
@@ -13,27 +13,30 @@ class History
     {
         $history = self::getHistory();
 
-        $lastPathInHistory = $history[count($history) - 1][0] ?? null;
-
         $currentPageName = Pages::getCurrentPageName();
         $currentPath = Pages::getPathForCurrentPage();
 
-        if ($currentPath === $lastPathInHistory) {
+        $addedItem = [$currentPath, $currentPageName];
+
+        $lastPath = $history[count($history) - 1][0] ?? null;
+
+        if ($lastPath === $currentPath) {
             return;
         }
 
-        // array_push($history, [$currentPath, $currentPageName]);
-        $history[] = [$currentPath, $currentPageName];
+        $checkedItemIndex = self::getHistorySize($history) === self::HISTORY_MAX_SIZE ? 1 : 0;
 
-        if (count($history) > self::HISTORY_SIZE) {
-            array_shift($history);
+        $checkedPath = array_slice($history, -2)[0][0];
+
+        $history[] = $addedItem;
+
+        if ($checkedPath === $currentPath) {
+            unset($history[$checkedItemIndex]);
+            self::saveHistory(array_values($history));
+            return;
         }
 
-        $firstPathInHistory = $history[0][0];
-
-        $containsRepeats = count($history) === self::HISTORY_SIZE && $currentPath === $firstPathInHistory;
-
-        if ($containsRepeats) {
+        if (self::getHistorySize($history) > self::HISTORY_MAX_SIZE) {
             array_shift($history);
         }
 
@@ -49,5 +52,10 @@ class History
     private static function saveHistory(array $history): void
     {
         setCookie('history', json_encode($history), 0, "/task2_1/");
+    }
+
+    public static function getHistorySize(array $history): int
+    {
+        return count($history);
     }
 }
