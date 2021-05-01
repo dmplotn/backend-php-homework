5e98a46f4fc7bcadb540298478f4dd087af8b185
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-01 23:18:43 +0500

    week4|add figure property validator

---
+++ web/app/Validators/FigurePropertyValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FigureCalculator\Validators;
+
+class FigurePropertyValidator
+{
+    /**
+     * @param float $property
+     *
+     * @return void
+     */
+    public static function validate(float $property): void
+    {
+        if ($property <= 0) {
+            throw new \DomainException("Property value is not positive. Value: {$property}.");
+        }
+    }
+
+    /**
+     * @param float ...$properties
+     *
+     * @return void
+     */
+    public static function validateMultipleProperties(float ...$properties): void
+    {
+        foreach ($properties as $property) {
+            self::validate($property);
+        }
+    }
+}
