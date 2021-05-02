ae9002fb4ea93f07dff25e5abc86e59eded62446
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-02 14:48:35 +0500

    week4|add figure calculator endpoint

---
+++ web/public/api/figureCalculator.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
+
+use FigureCalculator\{FigureFactory, FigureCalculator, RequestParamParser};
+use FigureCalculator\Exceptions\{
+    MissingRequestParamException,
+    ClassExistenceException,
+    PropertyExistenceException,
+    MethodExistenceException
+};
+
+$response = [];
+
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(404);
+    $response['status'] = 'error';
+    $response['message'] = 'Страница не найдена';
+    echo json_encode($response);
+    exit;
+}
+
+try {
+    [
+        'figure' => $figureName,
+        'operation' => $operationName,
+        'properties' => $properties
+    ] = RequestParamParser::parse($_GET);
+
+    $figure = FigureFactory::create($figureName, $properties);
+
+    $calculator = new FigureCalculator($figure);
+
+    $result = $calculator->calculate($operationName);
+} catch (
+    MissingRequestParamException |
+    ClassExistenceException |
+    PropertyExistenceException |
+    MethodExistenceException |
+    TypeError |
+    \DomainException $e
+) {
+    http_response_code(422);
+    $response['status'] = 'error';
+    $response['message'] = 'Запрос был отправлен с не правильным набором параметров.';
+    echo json_encode($response);
+    exit;
+}
+
+$response['status'] = 'success';
+$response['message'] = 'Запрос успешно выполнен';
+$response['result'] = $result;
+echo json_encode($response);
