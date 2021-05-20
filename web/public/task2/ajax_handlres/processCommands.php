7c5d8a8f87ec9f5015f3f81ff3e1e66a992b925a
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-20 19:59:17 +0500

    week5|(task2) add command process haldler

---
+++ web/public/task2/ajax_handlres/processCommands.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
+
+use Task2\Editor;
+use Task2\EditorCommandFactory;
+use Task2\EditorController;
+
+header('Content-Type: application/json');
+
+$response = [];
+
+if (!isset($_POST['commands'])) {
+    http_response_code(422);
+    $response['status'] = 'error';
+    $response['message'] = 'Отсуствуют параметры запроса.';
+    echo json_encode($response);
+    exit;
+}
+
+$rawCommands = $_POST['commands'];
+
+try {
+    $commands = EditorCommandFactory::create($rawCommands);
+    $controller = new EditorController();
+    $controller->process($commands);
+    $result = $controller->getEditorContent();
+} catch (\Exception $e) {
+    http_response_code(422);
+    $response['status'] = 'error';
+    $response['message'] = 'Команды не выполнены. Одна или несколько команд являются невалидным.';
+    echo json_encode($response);
+    exit;
+}
+
+$response['status'] = 'ok';
+$response['message'] = 'Команды успешно выполнены.';
+$response['result'] = $result;
+echo json_encode($response);
