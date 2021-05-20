36a5f50fb82600838afd58ba198e7e3cefc4fa3c
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-20 22:13:39 +0500

    week5|update index page

--- web/public/index.php
+++ web/public/index.php
@@ -1,27 +1,13 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
-
-use Task2\EditorCommandFactory;
-use Task2\EditorController;
-
-// $rawCommands = "insert \"helloo\" 4\ncopy 0 1\ninsert \"ll\" 2\ndelete 0 7\npaste 0"; //helloo -> helllloo -> hellll
-// $rawCommands = "insert \"HelloMyDearFriend\" 5\ncopy 0 4\npaste 17";
-// $rawCommands = "insert \"helloo\" 4";
-// $rawCommands = "insert \"t\" 0\ninsert \"ext\" 1";
-// $rawCommands = "undo";
-// “string”
-
-$rawCommands = <<<HERE
-insert "sdfsdfsdfsd" 0
-delete 0 3
-undo
-HERE;
-
-
-$commands = EditorCommandFactory::create($rawCommands);
-
-$controller = new EditorController();
-$controller->process($commands);
-var_dump($controller->getEditorContent());
-var_dump($controller->getHistory());
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Навигация</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/task1/index.php">Задание 1.</a></li>
+        <li><a href="/task2/index.php">Задание 2.</a></li>
+    </ul>
+</body>
+</html>
