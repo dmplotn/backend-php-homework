8f2817f688cb02ae27ddbe072b06e65cf171e8c6
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-25 20:08:30 +0500

    week6|sign up ajax handler

---
+++ web/public/ajax_handlers/signUp.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/db/init.php';
+
+use App\Validators\SignUpFormValidator;
+use App\Mappers\UserMapper;
+use App\Models\User;
+use App\Auth;
+
+
+session_start();
+
+$response = [];
+
+header('Content-Type: application/json');
+
+if (
+    !isset($_POST['login']) ||
+    !isset($_POST['password']) ||
+    !isset($_POST['passwordConfirmation'])
+) {
+    $response['status'] = 'error';
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+[
+    'login' => $login,
+    'password' => $password,
+    'passwordConfirmation' => $passwordConfirmation
+] = $_POST;
+
+$errors = SignUpFormValidator::validate($login, $password, $passwordConfirmation);
+
+if ($errors !== []) {
+    $response['status'] = 'error';
+    $response['errors'] = $errors;
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+$mapper = new UserMapper($pdo);
+$user = new User(null, $login, password_hash($password, PASSWORD_DEFAULT));
+
+try {
+    $mapper->insert($user);
+} catch (PDOException $e) {
+    $response['status'] = 'error';
+    $response['errors'][] = 'Пользователь с таким логином уже существует';
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+$authenticableUser = $mapper->getUserByLogin($login);
+Auth::signIn($authenticableUser);
+
+$response['status'] = 'success';
+echo json_encode($response);
+exit;
