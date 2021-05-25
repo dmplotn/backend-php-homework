85d84015fee7ff26dab6685357f1291d01cb8221
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-25 11:56:15 +0500

    week6|update index page

--- web/public/index.php
+++ web/public/index.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'Hello!';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/db/init.php';
+
+use App\Auth;
+use App\Mappers\UserMapper;
+
+session_start();
+
+if (!Auth::isUserSignedIn()) {
+    header('Location: /signIn.php');
+    exit;
+}
+
+$id = Auth::getSignedInUserId();
+$mapper = new UserMapper($pdo);
+$user = $mapper->getUserById($id);
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная страница</title>
+    <script src="scripts/signOut.js"></script>
+</head>
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php' ?>
+    <h1>Привет, <?= htmlspecialchars($user->getLogin()) ?>!</h1>
+</body>
+</html>
