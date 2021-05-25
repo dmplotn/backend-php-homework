b1b5d01027767551e474c5038ff171cc5fbd456a
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-25 10:55:41 +0500

    week6|add user model

---
+++ web/app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+class User
+{
+    /**
+     * @var int|null
+     */
+    private ?int $id = null;
+
+    /**
+     * @var string
+     */
+    private string $login;
+
+    /**
+     * @var string
+     */
+    private string $password;
+
+    /**
+     * @param int|null $id
+     * @param string $login
+     * @param string $password
+     */
+    public function __construct(?int $id, string $login, string $password)
+    {
+        $this->id = $id;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
