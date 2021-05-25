0594668e2dda58752224a210bbcbd5c4dc9ec306
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-25 20:11:00 +0500

    week6|add display errors js script

---
+++ web/public/scripts/errors.js
@@ -0,0 +1,22 @@
+const displayErrorContainer = (errors) => {
+  const ul = document.createElement('ul');
+  ul.className = 'list-group mb-5';
+  ul.id = 'errorContainer';
+  const listItems = errors.map((error) => {
+    const li = document.createElement("li");
+    li.className = 'list-group-item list-group-item-danger';
+    li.textContent = error;
+    return li;
+  });
+  ul.append(...listItems);
+
+  const target = document.getElementById('displayErrorsTarget');
+  target.before(ul);
+};
+
+const removeErrorContainer = () => {
+  const container = document.getElementById('errorContainer');
+  if (container) {
+    container.remove();
+  }
+}
