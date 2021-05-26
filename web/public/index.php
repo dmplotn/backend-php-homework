0655a1cdab16f1eb215f6e06130247b7b5934e99
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-26 20:20:48 +0500

    week6|update pages

--- web/public/index.php
+++ web/public/index.php
@@ -3,19 +3,17 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/db/init.php';
 
-use App\Auth;
-use App\Mappers\UserMapper;
+use App\UserFactory;
 
 session_start();
 
-if (!Auth::isUserSignedIn()) {
+$factory = new UserFactory($pdo);
+$user = $factory->getCurrentUser();
+
+if ($user->isGuest()) {
     header('Location: /signIn.php');
     exit;
 }
-
-$id = Auth::getSignedInUserId();
-$mapper = new UserMapper($pdo);
-$user = $mapper->getUserById($id);
 
 ?>
 <!DOCTYPE html>
@@ -23,35 +21,11 @@ $user = $mapper->getUserById($id);
 <head>
     <meta charset="UTF-8">
     <title>Главная страница</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <?php require 'inc/bootstrap.php' ?>
     <script src="scripts/signOut.js"></script>
 </head>
 <body>
-    <header class="mb-5">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-xl">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/index.php">Главная</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/userSettings.php?id=<?= $id ?>">Настройки пользователя</a>
-                        </li>
-                        <li class="nav-item">
-                            <form onsubmit="signOut(); return false"><button class="nav-link btn btn-link" type="submit">Выйти</button></form>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php require "inc/header.php"?>
     <section>
         <div class="container-xl">
             <h1 class="mb-5">Привет, <?= htmlspecialchars($user->getLogin()) ?>!</h1>
--- web/public/signUp.php
+++ web/public/signUp.php
@@ -3,11 +3,14 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/db/init.php';
 
-use App\Auth;
+use App\UserFactory;
 
 session_start();
 
-if (Auth::isUserSignedIn()) {
+$factory = new UserFactory($pdo);
+$user = $factory->getCurrentUser();
+
+if (!$user->isGuest()) {
     header('Location: /index.php');
     exit;
 }
@@ -18,34 +21,12 @@ if (Auth::isUserSignedIn()) {
 <head>
     <meta charset="UTF-8">
     <title>Регистрация пользователя</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <?php require 'inc/bootstrap.php' ?>
     <script src="scripts/signUp.js"></script>
     <script src="scripts/errors.js"></script>
 </head>
 <body>
-    <header id="displayErrorsTarget">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-xl">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/index.php">Главная</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/signIn.php">Авторизация</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php require "inc/header.php"?>
     <section class="mt-5">
         <div class="container-xl">
             <h1 class="mb-5">Регистрация пользователя</h1>
