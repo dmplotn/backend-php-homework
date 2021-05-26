120d13f63ae2e8f53bfa37a0050f5e00c34eaf68
Dmitry Plotnikov <dmplotn@gmail.com>
2021-05-26 16:29:08 +0500

    week6|add change user login ajax handler

---
+++ web/public/ajax_handlers/changeUserLogin.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/autoload/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../bootstrap/db/init.php';
+
+use App\Validators\ChangeLoginFormValidator;
+use App\Mappers\UserMapper;
+use App\Auth;
+
+session_start();
+
+header('Content-Type: application/json');
+
+if (!isset($_POST['newLogin']) || !isset($_POST['id'])) {
+    $response['status'] = 'error';
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+['newLogin' => $newLogin, 'id' => $id] = $_POST;
+
+$mapper = new UserMapper($pdo);
+
+$user = $mapper->getUserById($id);
+
+if (!$user) {
+    $response['status'] = 'error';
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+$currentLogin = $user->getLogin();
+
+$errors = ChangeLoginFormValidator::validate($currentLogin, $newLogin);
+
+if ($errors !== []) {
+    $response['status'] = 'error';
+    $response['errors'] = $errors;
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+$user->setLogin($newLogin);
+
+try {
+    $mapper->update($user);
+} catch (PDOException $e) {
+    $response['status'] = 'error';
+    $response['errors'][] = 'Пользователь с таким логином уже существует';
+    http_response_code(422);
+    echo json_encode($response);
+    exit;
+}
+
+Auth::signOut();
+
+$response['status'] = 'success';
+echo json_encode($response);
+exit;
