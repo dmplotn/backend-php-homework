d69bffc56bbf9396f906b87210322ffe47c53fd6
Dmitry Plotnikov <dmplotn@gmail.com>
2021-06-02 12:49:00 +0500

    week7|add symfony console

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     WouterJ\EloquentBundle\WouterJEloquentBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
