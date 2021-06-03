cb484517b591324c63eeace43cae3b4e458784b2
Dmitry Plotnikov <dmplotn@gmail.com>
2021-06-03 14:59:12 +0500

    week7|add migrations

---
+++ web/migrations/2021_06_03_084222_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use WouterJ\EloquentBundle\Facade\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('iso', 2);
+            $table->foreignId('currency_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
