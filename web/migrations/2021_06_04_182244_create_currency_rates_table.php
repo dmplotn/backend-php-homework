2c8bb4aebaf0287d331b96c779a1393c60ef3c43
Dmitry Plotnikov <dmplotn@gmail.com>
2021-06-04 18:42:47 +0500

    week7|add migration

---
+++ web/migrations/2021_06_04_182244_create_currency_rates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use WouterJ\EloquentBundle\Facade\Schema;
+
+class CreateCurrencyRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency_rates', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('currency_rate', 15, 4);
+            $table->date('date');
+            $table->foreignId('currency_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency_rates');
+    }
+}
