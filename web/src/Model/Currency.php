7cb643b199807f01e191ecafb49629ee8181bd85
Dmitry Plotnikov <dmplotn@gmail.com>
2021-06-07 20:41:17 +0500

    week7|update currency model

--- web/src/Model/Currency.php
+++ web/src/Model/Currency.php
@@ -3,11 +3,39 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use WouterJ\EloquentBundle\Facade\Db;
+use App\Model\CurrencyRate;
 
 class Currency extends Model
 {
     public function countries()
     {
         return $this->hasMany(Country::class);
+    }
+
+    public static function currentRatesData()
+    {
+        $latestCurrencyRates = Db::table('currency_rates')
+            ->selectRaw('currency_id, max(date) as date')
+            ->groupBy('currency_id');
+
+        return Db::table('currencies')
+            ->select(
+                'currencies.id as currency_id',
+                'currencies.iso as currency_iso',
+                'currencies.cbrf_id as currency_cbrf_id',
+                'countries.id as country_id',
+                'countries.iso as country_iso',
+                'currency_rates.currency_rate',
+                'currency_rates.date'
+            )
+            ->join('countries', 'currencies.id', '=', 'countries.currency_id')
+            ->join('currency_rates', 'currencies.id', '=', 'currency_rates.currency_id')
+            ->joinSub($latestCurrencyRates, 'latest_currency_rates', function ($join) {
+                $join
+                    ->on('currency_rates.currency_id', '=', 'latest_currency_rates.currency_id')
+                    ->on('currency_rates.date', '=', 'latest_currency_rates.date');
+            })
+            ->get();
     }
 }
