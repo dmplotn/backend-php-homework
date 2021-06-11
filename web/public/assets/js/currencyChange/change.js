ffc254b26e6be9d92959530fc705fd16813c93bc
Dmitry Plotnikov <dmplotn@gmail.com>
2021-06-11 13:09:06 +0500

    week7|add order form

--- web/public/assets/js/currencyChange/change.js
+++ web/public/assets/js/currencyChange/change.js
@@ -17,10 +17,12 @@ const format = (value) => {
   return formatter.format(value).replace(',', '.');
 };
 
-const render = (state) => {
+const renderConverter = (state) => {
   const changeResult = document.getElementById('change-result');
   const changeValueInput = document.getElementById('change-value-input');
   const changeValueIso = document.getElementById('change-value-iso');
+  const changeResultInput = document.getElementById('change-result-input');
+  const orderSubmit = document.getElementById('order-submit');
 
   changeValueIso.textContent = state.changeValueIso;
 
@@ -30,33 +32,147 @@ const render = (state) => {
     changeValueInput.value = '';
   }
   
-  if (!state.changeValueInputValid) {
+  if (!state.changeValueInput.valid) {
     changeValueInput.classList.add('is-invalid');
+    const changeValueInputError = document.getElementById('change-value-input-error');
+    changeValueInputError.textContent = state.changeValueInput.error;
+    orderSubmit.setAttribute('disabled', 'disabled');
   } else {
     changeValueInput.classList.remove('is-invalid');
+    orderSubmit.setAttribute('disabled', 'disabled');
 
     if (state.value !== null) {
       const result = state.operation(state.value, state.rateData.currencyRate);
       const formattedResult = format(result);
       const currencyIso = state.operationType === 'sell' ? 'RUB' : state.rateData.currencyIso;
       changeResult.textContent = `Вы получите: ${formattedResult} ${currencyIso}`; 
+      changeResultInput.value = result;
+      orderSubmit.removeAttribute('disabled');
     }
   }
 }
 
-const validateChangeValueInput = (rawValue) => {
+const restoreChangeForm = (state) => {
+  if (!state.orderNameInput.error) {
+    state.orderNameInput.valid = true;
+  }
+  const orderNameInput = document.getElementById('order-name-input');
+  orderNameInput.classList.remove('is-invalid');
+
+  if (!state.orderEmailInput.error) {
+    state.orderEmailInput.valid = true;
+  }
+  const orderEmailInput = document.getElementById('order-email-input');
+  orderEmailInput.classList.remove('is-invalid');
+
+  if (!state.orderPhoneNumberInput.error) {
+    state.orderPhoneNumberInput.valid = true;
+  }
+  const orderPhoneNumberInput = document.getElementById('order-phone-number-input');
+  orderPhoneNumberInput.classList.remove('is-invalid');
+}
+
+const renderChangeForm = (state) =>  {
+  restoreChangeForm(state);
+
+  if (!state.orderNameInput.valid) {
+    const orderNameInput = document.getElementById('order-name-input');
+    orderNameInput.classList.add('is-invalid');
+
+    const orderNameError = document.getElementById('order-name-error');
+    orderNameError.textContent = state.orderNameInput.error;
+  }
+
+  if (!state.orderEmailInput.valid) {
+    const orderEmailInput = document.getElementById('order-email-input');
+    orderEmailInput.classList.add('is-invalid');
+
+    const orderEmailError = document.getElementById('order-email-error');
+    orderEmailError.textContent = state.orderEmailInput.error;
+  }
+
+  if (!state.orderPhoneNumberInput.valid) {
+    const orderPhoneNumberInput = document.getElementById('order-phone-number-input');
+    orderPhoneNumberInput.classList.add('is-invalid');
+
+    const orderPhoneNumberError = document.getElementById('order-phone-number-error');
+    orderPhoneNumberError.textContent = state.orderPhoneNumberInput.error;
+  }
+};
+
+const validateChangeValueInput = (state) => {
   const pattern = /^[0-9]+(\.[0-9]{1,2})?$/;
   
-  if (!rawValue.match(pattern)) {
+  if (state.changeValueInput.value === '') {
+    return true;
+  }
+
+  if (!state.changeValueInput.value.match(pattern)) {
+    state.changeValueInput.error = 'Значение не соостветствует формату валюты';
     return false;
   }
 
-  const value = parseFloat(rawValue);
-  return value > 0 && value <= 1000000;
+  const value = parseFloat(state.changeValueInput.value);
+
+  if (value <= 0 || value > 1000000) {
+    state.changeValueInput.error = 'Значение должено быть от 0 до 1 млн';
+    return false;
+  }
+
+  return true;
+};
+
+const validateOrderNameInput = (state) => {
+  const pattern = /^[a-z ,.'-]+$/;
+
+  if (!state.orderNameInput.value.match(pattern)) {
+    state.orderNameInput.valid = false;
+    state.orderNameInput.error = 'Значение не соостветствует имени';
+    return false;
+  }
+
+  state.orderNameInput.valid = true;
+  return true;
+};
+
+const validateOrderEmailInput = (state) => {
+  const pattern = /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/;
+
+  if (!state.orderEmailInput.value.match(pattern)) {
+    state.orderEmailInput.valid = false;
+    state.orderEmailInput.error = 'Значение не соостветствует формату email';
+    return false;
+  }
+
+  state.orderEmailInput.valid = true;
+  return true;
+};
+
+const validateOrderPhoneNumberInput = (state) => {
+  const pattern = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+
+  if (!state.orderPhoneNumberInput.value.match(pattern)) {
+    state.orderPhoneNumberInput.valid = false;
+    state.orderPhoneNumberInput.error = 'Значение не соостветствует формату тел.номера';
+    return false;
+  }
+
+  state.orderPhoneNumberInput.valid = true;
+  return true;
+};
+
+const validateChangeForm = (state) => {
+  const validationNameResult = validateOrderNameInput(state);
+  const validationEmailResult = validateOrderEmailInput(state);
+  const validationPhoneResult = validateOrderPhoneNumberInput(state);
+
+  return validationNameResult && validationEmailResult && validationPhoneResult;
 };
 
 const change = () => {
   const currentRatesData = getCurrentRatesData();
+
+  const changeForm = document.getElementById('change-form');
 
   const operationSelect = document.getElementById('operation');
   const operationType = operationSelect[operationSelect.selectedIndex].value;
@@ -69,29 +185,38 @@ const change = () => {
   const changeValueInput = document.getElementById('change-value-input');
   const changeValueIso = operationType === 'sell' ? rateData.currencyIso : 'RUB';
 
+  const orderNameInput = document.getElementById('order-name-input');
+  const orderPhoneNumberInput = document.getElementById('order-phone-number-input');
+  const orderEmailInput = document.getElementById('order-email-input');
+
   const state = {
     value: null,
     rateData,
     operationType,
     operation,
     changeValueIso,
-    changeValueInputValid: true,
     afterSelect: true,
+    changeValueInput: {
+      valid: true,
+    },
+    orderNameInput: {},
+    orderPhoneNumberInput: {},
+    orderEmailInput: {}
   };
 
   changeValueInput.addEventListener('keyup', (e) => {
-    const rawValue = changeValueInput.value;
-    
-    if (validateChangeValueInput(rawValue) || rawValue === '') {
-      state.changeValueInputValid = true;
-      state.value = rawValue === '' ? null : parseFloat(rawValue);
+    state.changeValueInput.value = changeValueInput.value;
+
+    if (validateChangeValueInput(state)) {
+      state.changeValueInput.valid = true;
+      state.value = state.changeValueInput.value === '' ? null : parseFloat(state.changeValueInput.value);
     } else {
-      state.changeValueInputValid = false;
+      state.changeValueInput.valid = false;
     }
 
     state.afterSelect = false;
 
-    render(state);
+    renderConverter(state);
   });
 
   operationSelect.addEventListener('change', (e) => {
@@ -102,15 +227,13 @@ const change = () => {
     const changeValueIso = operationType === 'sell' ? rateData.currencyIso : 'RUB';
 
     state.value = null;
-    state.changeValueInputValid = true;
+    state.changeValueInput.valid = true;
     state.changeValueIso = changeValueIso;
     state.operationType = operationType;
     state.operation = operation;
     state.afterSelect = true;
 
-    console.log(state);
-
-    render(state);
+    renderConverter(state);
   });
 
   currencySelect.addEventListener('change', (e) => {
@@ -120,14 +243,26 @@ const change = () => {
 
     state.value = null;
     state.changeValueIso = changeValueIso;
-    state.changeValueInputValid = true;
+    state.changeValueInput.valid = true;
     state.rateData = rateData;
     state.afterSelect = true;
 
-    render(state);
+    renderConverter(state);
   });
 
-  render(state);
+  changeForm.addEventListener('submit', (e) => {
+    state.orderNameInput.value = orderNameInput.value;
+    state.orderPhoneNumberInput.value = orderPhoneNumberInput.value;
+    state.orderEmailInput.value = orderEmailInput.value;
+
+    if (!validateChangeForm(state)) {
+      renderChangeForm(state);
+      e.preventDefault();
+      return false;
+    }
+  });
+
+  renderConverter(state);
 };
 
 change();
